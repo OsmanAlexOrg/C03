7c22bccbd0a81e7637b4a33e14e56ab18b55a925
amessner <alexander.messner.am@gmail.com>
2017-02-27 10:47:48 +0100

    HALLO

--- src/main.java
+++ src/main.java
@@ -1,8 +1,13 @@
 
 public class main {
-
+	public static void GrußvonAlex(){
+		System.out.println("Hallo Osman");
+		System.out.println("Hallo ich bin Alex");
+	}
 	public static void main(String[] args) {
 		System.out.println("Schöne Grüße von Osman");
+		GrußvonAlex();
+		GrußvonAlex();
 
 	}
 
